f1ef4e3a89e1c76598e84491ce9112117013c772
olkin <pilip.olkin@gmail.com>
2020-05-12 16:17:04 -0600

    add Products as React

---
+++ app/javascript/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Products from "./Products";
+
+const Main = () => {
+    return (
+        <>
+            <h1>Products</h1>
+            <Products />
+        </>
+    );
+}
+
+export default Main;
+
