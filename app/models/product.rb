f049617bf4d45b5c710a475542208c9478853677
olkin <pilip.olkin@gmail.com>
2020-05-12 15:43:03 -0600

    more seeds

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_blank: true
 
   belongs_to :department
+
+  # maybe 1 product can have only 1 promotion? I'm not sure from data provided
   has_and_belongs_to_many :promotions
   has_and_belongs_to_many :active_promotions, -> { active }, class_name: 'Promotion'
 end
--- db/seeds/product_with_promotions_seeds.rb
+++ db/seeds/product_with_promotions_seeds.rb
@@ -3,15 +3,18 @@ active_promotion2 = Promotion.active.first
 deactivated_promotion = Promotion.deactivated.last
 
 Product.all.find_each do |product|
+  if deactivated_promotion && product.id % 15 == 0 && !product.promotions.exists?(deactivated_promotion.id)
+    product.promotions << deactivated_promotion
+    next
+  end
+
   if active_promotion && product.id % 6 == 0 && !product.promotions.exists?(active_promotion.id)
     product.promotions << active_promotion
+    next
   end
 
   if active_promotion2 && product.id % 5 == 0 && !product.promotions.exists?(active_promotion2.id)
     product.promotions << active_promotion2
-  end
-
-  if deactivated_promotion && product.id % 15 == 0 && !product.promotions.exists?(deactivated_promotion.id)
-    product.promotions << deactivated_promotion
+    next
   end
 end
