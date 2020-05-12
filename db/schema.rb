72e6b66e6b833ebed27dc9841d27225d4450bf18
olkin <pilip.olkin@gmail.com>
2020-05-12 14:24:46 -0600

    add promotions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_12_195513) do
+ActiveRecord::Schema.define(version: 2020_05_12_202247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2020_05_12_195513) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["department_id"], name: "index_products_on_department_id"
+  end
+
+  create_table "promotions", force: :cascade do |t|
+    t.string "code"
+    t.boolean "active"
+    t.decimal "discount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
