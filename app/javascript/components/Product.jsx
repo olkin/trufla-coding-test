95c95cbae0d98cbf1c08fc0a346d8a50b414e997
olkin <pilip.olkin@gmail.com>
2020-05-12 18:48:43 -0600

    add promotion data

--- app/javascript/components/Product.jsx
+++ app/javascript/components/Product.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 
 const Product = (props) => {
+    const discountedPrice = props.promotion ? (props.price - props.price * props.promotion.discount/100).toFixed(2) : props.price;
+
+    const discount = props.promotion ? `${props.promotion.discount}%` : '';
+
     return (
-        <div>{props.name} ${props.price} {props.promotion?.code}</div>
+        <div>
+            {props.name} ${props.price} {props.promotion?.code} {discount} ${discountedPrice}
+        </div>
     );
 }
 
