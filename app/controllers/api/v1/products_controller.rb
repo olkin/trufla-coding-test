34bbfd6f260c66da54206ee7dbb09ca757df591c
olkin <pilip.olkin@gmail.com>
2020-05-13 09:48:57 -0600

    Filtering by promo code

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::ProductsController < Api::V1::BaseController
   def index
     @products = Product.order(created_at: :desc)
     @products = @products.where(department_id: params[:department_id]) if params[:department_id].present?
+    @products = @products.where(promotions: { code: params[:promo_code] }) if params[:promo_code].present?
     @products = @products.includes(:active_promotions, :department)
     @products = @products.page(params[:page]).per(PER_PAGE)
 
