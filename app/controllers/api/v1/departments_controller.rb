40dc1452660148c8b2f378f02a2ba90ee662cbce
olkin <pilip.olkin@gmail.com>
2020-05-13 13:08:50 -0600

    remove unnecessary comment

--- app/controllers/api/v1/departments_controller.rb
+++ app/controllers/api/v1/departments_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::DepartmentsController < ApplicationController
   def index
-    # Hopefully not that many departments
     @departments = Department.all.order(:name)
     render json: @departments
   end
