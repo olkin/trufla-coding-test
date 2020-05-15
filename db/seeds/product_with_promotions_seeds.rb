c19078e9670e7f2839ec50458d7639ab8075a897
olkin <pilip.olkin@gmail.com>
2020-05-15 14:03:48 -0600

    only add promos to code if does not  exist

--- db/seeds/product_with_promotions_seeds.rb
+++ db/seeds/product_with_promotions_seeds.rb
@@ -3,17 +3,19 @@ active_promotion2 = Promotion.active.first
 deactivated_promotion = Promotion.deactivated.last
 
 Product.all.find_each do |product|
-  if deactivated_promotion && product.id % 15 == 0 && !product.promotions.exists?(deactivated_promotion.id)
+  next if product.promotions.any?
+
+  if deactivated_promotion && product.id % 15 == 0
     product.promotions << deactivated_promotion
     next
   end
 
-  if active_promotion && product.id % 6 == 0 && !product.promotions.exists?(active_promotion.id)
+  if active_promotion && product.id % 6 == 0
     product.promotions << active_promotion
     next
   end
 
-  if active_promotion2 && product.id % 5 == 0 && !product.promotions.exists?(active_promotion2.id)
+  if active_promotion2 && product.id % 5 == 0
     product.promotions << active_promotion2
     next
   end
